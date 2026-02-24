7560932bd7f345fc55fe31a653917d2120afa9fe
Mohamed Abdullah <moh.abdullah.it@gmail.com>
2026-02-24 13:52:18 +0300

    [MOD]: TEST BUILD AND RUN EXAMPLE

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/opt/homebrew/Caskroom/flutter/2.8.1/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/moahmedabdullah/Desktop/apps/app_features"
+export "FLUTTER_ROOT=/opt/homebrew/Caskroom/flutter/3.27.3/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/mohamedabdalla/Desktop/apps/app_features"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=0.6.0"
-export "FLUTTER_BUILD_NUMBER=0.6.0"
+export "FLUTTER_BUILD_NAME=1.0.2"
+export "FLUTTER_BUILD_NUMBER=1.0.2"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
